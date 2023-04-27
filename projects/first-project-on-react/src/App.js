653f1ec110e507ec9cb01239639d1a5e82d6ffb4
6ivkin <fedorterentev48@gmail.com>
2023-04-27 16:51:00 +0400

    Реализовал удаление постов, добавление постов

---
+++ projects/first-project-on-react/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import PostForm from './components/PostForm'
+import PostList from './components/PostList'
+import './styles/App.css'
+
+function App() {
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'JavaScript', body: 'Description' },
+		{ id: 2, title: 'JavaScript 2', body: 'Description' },
+		{ id: 3, title: 'JavaScript 3', body: 'Description' },
+	])
+
+	const createPost = newPost => {
+		setPosts([...posts, newPost])
+	}
+
+	// Получаем post из дочернего компонента
+	const removePost = post => {
+		setPosts(posts.filter(p => p.id !== post.id))
+	}
+
+	return (
+		<div className='App'>
+			<PostForm create={createPost} />
+			<PostList remove={removePost} posts={posts} title='Посты про JS' />
+		</div>
+	)
+}
+
+export default App
